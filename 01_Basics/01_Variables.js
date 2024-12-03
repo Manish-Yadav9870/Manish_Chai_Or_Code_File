8dcdc43d4e5932080ca501445c9c1d18fe9815cd
Manish Yadav <158013743+Manish-Yadav9870@users.noreply.github.com>
2024-12-03 11:42:19 +0000

    update msg

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,53 @@
+// const accountId =  144553
+// let accountEmail = "myadav9870@gmail.com"
+// var accountPass = "12345"
+// accountCity = "jaipur"
+// // accountId = 2
+// let accountState;
+
+// accountEmail = "my@gmail.com"
+// accountPass = "212121"
+// accountCity = "Bangluru"
+
+// console.log(accountId);
+// console.table([accountId,accountEmail,accountPass,accountCity,accountState])
+
+
+
+
+// *--------------------- Var, Let and Const Variables Examples---------------------------/
+
+
+// 1st var:- the var is the oldest keyword to decleared a vaiable in javascript.
+
+// var is globel scoped and function scoped
+
+// var a = 10;
+// function f(){
+//     var b = 20;
+//     console.log(a,b);
+    
+// }
+// f();
+// console.log(a);
+
+
+// function f (){
+//     var a =10;
+//     console.log(a);
+    
+// }
+// f();
+// // console.log(a);
+// it answer is 10
+
+
+// console.log(a)
+// var a = 10
+// it answer is undefined
+
+
+
+// let:- let Variables has the block Scope
+
+
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"//treat all JS code as newer version
+
+// alert(3 + 3)// we are using nodejs, not browser
+
+// console.log(3+3);
+// console.log("Manish");code readability should be high
+
+
+let name = "manish";
+let age = 18
+let isLoggedIn = false
+
+
+
+
